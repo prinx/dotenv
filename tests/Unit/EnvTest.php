f5ad457f7df8cc45181fca66db3fd794cc56a57e
CinPer <damien.dorcis@yahoo.com>
2020-07-09 21:32:01 +0000

    Update EnvTest.php

--- tests/Unit/EnvTest.php
+++ tests/Unit/EnvTest.php
@@ -15,10 +15,17 @@ use Prinx\Dotenv\Dotenv;
  */
 class EnvTest extends TestCase
 {
+    protected $envFile = '';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->envFile = realpath(__DIR__ . '/../../.env.example');
+    }
 
     public function testLoadEnv()
     {
-        loadEnv(realpath(__DIR__ . '/../../.env'));
+        loadEnv($this->envFile);
         $this->assertTrue(is_a(dotenv(), Dotenv::class), 'Test Env load');
     }
 
@@ -37,16 +44,17 @@ class EnvTest extends TestCase
     {
         // The test will modify the env file.
         // We Save the content of the file to bde able to revert it back later
-        $envFile = dotenv()->getPath();
-        $content = file_get_contents($envFile);
+        $content = file_get_contents($this->envFile);
 
         // Test
         persistEnv('PERSISTENCE', 'all_good');
-        //Reload the env to get all the variables from the env file instead of the memory-cached env array
-        loadEnv(realpath(__DIR__ . '/../../.env'));
+
+        // Reload the env to get all the variables from the env file instead
+        // of the memory-cached env array
+        loadEnv($this->envFile);
         $this->assertEquals('all_good', env('PERSISTENCE'), 'persist env variable PERSISTENCE');
 
         // Revert the file back to its state
-        file_put_contents($envFile, $content);
+        file_put_contents($this->envFile, $content);
     }
 }
